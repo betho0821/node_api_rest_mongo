f518854049646162afda0b1b9a6167940d662dd3
beto <betho0821@gmail.com>
2024-07-01 14:21:41 -0600

    realizacion de un servidor API REST con Mongo y Express

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { config } = require('dotenv');
+config();
+
+const bookRoutes = require('./routes/book.routes');
+
+// Usamos express para los middlewares
+const app = express();
+app.use(bodyParser.json()); // Parseador de bodies
+
+// Conectamos la base de datos
+mongoose.connect(process.env.MONGO_URL, { dbName: process.env.MONGO_DB_NAME });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Conectado a MongoDB');
+});
+
+// Usamos las rutas
+app.use('/books', bookRoutes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado en el puerto ${port}`);
+});
